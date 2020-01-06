5cb51e63079877144c5fdc9c1439710add6168ab
cwalbanese <631gti@gmail.com>
2020-01-06 16:23:09 -0500

    Logic for first two questions
    Wrote in javascript logic to make questions one and two functional

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const startingLine = document.querySelector('.starting-line');
 const question = document.querySelector('.question');
 const answersList = document.querySelector('.answer-list');
 const cars = ['Audi', 'BMW', 'Tesla', 'Volkswagen'];
+const score = document.querySelector('.current-score');
+let newButton = document.createElement('button');
+const carsTwo = ['Lexus', 'Ferrari', 'Jaguar', 'Kia'];
+let listItem = document.getElementsByTagName('li');
 
 reset.addEventListener('click', reloadPage);
 start.addEventListener('click', questionOne);
@@ -14,7 +18,7 @@ function reloadPage() {
 
 function questionOne(evt) {
   evt.preventDefault();
-  startingLine.style.opacity = 0;
+  startingLine.style.visibility = 'hidden';
   question.innerText =
     'Question 1: Which car company is leading in electric technology?';
   for (i = 0; i < 4; i++) {
@@ -22,5 +26,53 @@ function questionOne(evt) {
     answersList.appendChild(answer);
     answer.innerText = cars[i];
     answer.classList = cars[i];
+    answersList.addEventListener('click', answerOne);
+  }
+}
+
+function answerOne(evt) {
+  if (evt.target.className === 'Tesla') {
+    question.innerText =
+      'Correct! Tesla is leading the pack in the electric vehicle industry.';
+    score.innerText = parseInt(score.innerText) + 1;
+    question.appendChild(newButton);
+    newButton.innerText = 'NEXT QUESTION';
+    newButton.classList = 'next';
+    newButton.addEventListener('click', questionTwo);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is Tesla!';
+    question.appendChild(newButton);
+    newButton.innerText = 'NEXT QUESTION';
+    newButton.classList = 'next';
+    newButton.addEventListener('click', questionTwo);
+  }
+}
+
+function questionTwo(evt) {
+  evt.preventDefault();
+  question.innerText = 'Question 2: Which car company is built in Italy?';
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsTwo[i];
+    listItem[i].classList = carsTwo[i];
+    answersList.addEventListener('click', answerTwo);
+  }
+}
+
+function answerTwo(evt) {
+  if (evt.target.className === 'Ferrari') {
+    question.innerText = 'Correct! Ferrari are built and designed in Italy!';
+    score.innerText = parseInt(score.innerText) + 1;
+    question.appendChild(newButton);
+    newButton.innerText = 'NEXT QUESTION';
+    newButton.classList = 'next';
+    newButton.addEventListener('click', questionThree);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is Ferrari!';
+    question.appendChild(newButton);
+    newButton.innerText = 'NEXT QUESTION';
+    newButton.classList = 'next';
+    newButton.addEventListener('click', questionThree);
   }
 }
