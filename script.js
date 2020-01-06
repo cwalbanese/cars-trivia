1188e0c4347542c5e68897ef4a59f0131dfd160b
cwalbanese <631gti@gmail.com>
2020-01-06 15:20:45 -0500

    Question 1
    Started building javascript to create question 1 with answers

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const reset = document.querySelector('.reset');
+const start = document.querySelector('.start');
+const startingLine = document.querySelector('.starting-line');
+const question = document.querySelector('.question');
+const answersList = document.querySelector('.answer-list');
+const cars = ['Audi', 'BMW', 'Tesla', 'Volkswagen'];
 
 reset.addEventListener('click', reloadPage);
+start.addEventListener('click', questionOne);
 
 function reloadPage() {
   location.reload();
+}
+
+function questionOne(evt) {
+  evt.preventDefault();
+  startingLine.style.opacity = 0;
+  question.innerText =
+    'Question 1: Which car company is leading in electric technology?';
+  for (i = 0; i < 4; i++) {
+    let answer = document.createElement('li');
+    answersList.appendChild(answer);
+    answer.innerText = cars[i];
+    answer.classList = cars[i];
+  }
 }
