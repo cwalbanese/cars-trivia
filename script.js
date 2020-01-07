9db72c657dac476f2ae7fdf96d5e69f729515356
cwalbanese <631gti@gmail.com>
2020-01-07 12:43:17 -0500

    All trivia questions functions
    Completed code for all 10 trivia questions. Tested and found operational.

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const score = document.querySelector('.current-score');
 let newButton = document.createElement('button');
 const carsTwo = ['Lexus', 'Ferrari', 'Jaguar', 'Kia'];
 const carsThree = ['GMC', 'Chevrolet', 'Ford', 'Dodge'];
+const carsFour = ['Audi', 'Volkswagen', 'Porsche', 'Lamborghini'];
+const carsFive = ['1912', '1924', '1900', '1908'];
+const carsSix = ['engine', 'transmission', 'differential', 'throttle'];
+const carsSeven = ['Italy', 'Spain', 'Germany', 'France'];
+const carsEight = ['300 Million', '1 Billion', '500 Million', '600 Million'];
+const carsNine = ['1948', '1932', '1965', '1954'];
+const carsTen = ['8', '10', '12', '16'];
 let listItem = document.getElementsByTagName('li');
 
 reset.addEventListener('click', reloadPage);
@@ -23,7 +30,7 @@ function nextButton() {
   newButton.classList = 'next';
 }
 
-function questionOne(evt) {
+function questionOne() {
   startingLine.style.visibility = 'hidden';
   question.innerText =
     'Question 1: Which car company is leading in electric technology?';
@@ -39,7 +46,7 @@ function questionOne(evt) {
 function answerOne(evt) {
   if (evt.target.className === 'Tesla') {
     question.innerText =
-      'Correct! Tesla is leading the pack in the electric vehicle industry.';
+      'Correct! Tesla is leading in the electric vehicle industry.';
     score.innerText = parseInt(score.innerText) + 1;
     nextButton();
     newButton.addEventListener('click', questionTwo);
@@ -51,7 +58,7 @@ function answerOne(evt) {
   }
 }
 
-function questionTwo(evt) {
+function questionTwo() {
   question.innerText = 'Question 2: Which car company is built in Italy?';
   for (i = 0; i < 4; i++) {
     listItem[i].innerText = carsTwo[i];
@@ -81,12 +88,12 @@ function hover(evt) {
     evt.target.style.color = '#D93A2B';
     setTimeout(function() {
       evt.target.style.color = '';
-    }, 500);
+    }, 750);
   }
   false;
 }
 
-function questionThree(evt) {
+function questionThree() {
   question.innerText = `Question 3: Which car company's logo is commonly referred to as "the bowtie"?`;
   for (i = 0; i < 4; i++) {
     listItem[i].innerText = carsThree[i];
@@ -106,5 +113,162 @@ function answerThree(evt) {
       'Incorrect, the correct answer we are looking for is Chevrolet!';
     nextButton();
     newButton.addEventListener('click', questionFour);
+  }
+}
+
+function questionFour() {
+  question.innerText = `Question 4: Which car company owns the other three?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsFour[i];
+    listItem[i].classList = carsFour[i];
+    answersList.addEventListener('click', answerFour);
+  }
+}
+
+function answerFour(evt) {
+  if (evt.target.className === 'Volkswagen') {
+    question.innerText = `Correct! Volkswagen owns many car manufacturers!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionFive);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is Volkswagen!';
+    nextButton();
+    newButton.addEventListener('click', questionFive);
+  }
+}
+
+function questionFive() {
+  question.innerText = `Question 5: In what year did Henry Ford create the iconic Model T?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsFive[i];
+    listItem[i].classList = carsFive[i];
+    answersList.addEventListener('click', answerFive);
+  }
+}
+
+function answerFive(evt) {
+  if (evt.target.className === '1908') {
+    question.innerText = `Correct! 1908 is the year of the Model T!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionSix);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is 1908!';
+    nextButton();
+    newButton.addEventListener('click', questionSix);
+  }
+}
+
+function questionSix() {
+  question.innerText = `Question 6: Your car's horsepower comes from where?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsSix[i];
+    listItem[i].classList = carsSix[i];
+    answersList.addEventListener('click', answerSix);
+  }
+}
+
+function answerSix(evt) {
+  if (evt.target.className === 'engine') {
+    question.innerText = `Correct! The engine makes all that horsepower!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionSeven);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is engine!';
+    nextButton();
+    newButton.addEventListener('click', questionSeven);
+  }
+}
+
+function questionSeven() {
+  question.innerText = `Question 7: The Nurburgring is a famous racetrack in what country?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsSeven[i];
+    listItem[i].classList = carsSeven[i];
+    answersList.addEventListener('click', answerSeven);
+  }
+}
+
+function answerSeven(evt) {
+  if (evt.target.className === 'Germany') {
+    question.innerText = `Correct! The Nurburgring is in Germany!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionEight);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is Germany!';
+    nextButton();
+    newButton.addEventListener('click', questionEight);
+  }
+}
+
+function questionEight() {
+  question.innerText = `Question 8: How many cars are estimated to be on Earth?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsEight[i];
+    listItem[i].classList = carsEight[i];
+    answersList.addEventListener('click', answerEight);
+  }
+}
+
+function answerEight(evt) {
+  if (evt.target.className === '1 Billion') {
+    question.innerText = `Correct! Over 1 Billion Cars!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionNine);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is 1 Billion!';
+    nextButton();
+    newButton.addEventListener('click', questionNine);
+  }
+}
+
+function questionNine() {
+  question.innerText = `Question 9: In what year was NASCAR founded?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsNine[i];
+    listItem[i].classList = carsNine[i];
+    answersList.addEventListener('click', answerNine);
+  }
+}
+
+function answerNine(evt) {
+  if (evt.target.className === '1948') {
+    question.innerText = `Correct! NASCAR was founded in 1948!`;
+    score.innerText = parseInt(score.innerText) + 1;
+    nextButton();
+    newButton.addEventListener('click', questionTen);
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is 1948!';
+    nextButton();
+    newButton.addEventListener('click', questionTen);
+  }
+}
+
+function questionTen() {
+  question.innerText = `Question 10: The supercar Bugatti Veyron has how many engine cylinders?`;
+  for (i = 0; i < 4; i++) {
+    listItem[i].innerText = carsTen[i];
+    listItem[i].classList = carsTen[i];
+    answersList.addEventListener('click', answerTen);
+  }
+}
+
+function answerTen(evt) {
+  if (evt.target.className === '16') {
+    question.innerText = `Correct! The Bugatti Veyron has a 16 cylinder engine! Wow!`;
+    score.innerText = parseInt(score.innerText) + 1;
+  } else {
+    question.innerText =
+      'Incorrect, the correct answer we are looking for is 16!';
   }
 }
